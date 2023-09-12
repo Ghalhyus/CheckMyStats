0cd3454591c4bc694ca789581d7770f9e85077e2
SIMONIN David <ghalhyus@gmail.com>
2023-09-12 19:01:16 +0200

    First commit

---
+++ src/main/java/com/ghalhyus/web/CheckMyStats/CheckMyStatsApplication.java
@@ -0,0 +1,13 @@
+package com.ghalhyus.web.CheckMyStats;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CheckMyStatsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CheckMyStatsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/ghalhyus/web/CheckMyStats/CheckMyStatsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ghalhyus.web.CheckMyStats;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CheckMyStatsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
